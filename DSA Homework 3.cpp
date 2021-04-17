43b0021ef2e2407b0fc8573f97b32806fc886889
KKroliKK <a.vagin@innopolis.university>
2021-04-17 19:30:39 +0300

    Graph ADT interface

--- DSA Homework 3.cpp
+++ DSA Homework 3.cpp
@@ -17,16 +17,47 @@
 #include <mutex>
 #include <dos.h>
 #include "windows.h"
+#include "list"
 
 using namespace std;
 
+template <typename T> class Vertex {
+
+    T value;
+
+};
+
+
+template <typename T> class Edge {
+
+    T weight;
+
+};
+
+template <typename V, typename E> class GraphADT {
+
+    virtual Vertex<V>* addVertex(V vertex) = 0;
+    virtual void removeVertex(Vertex<V>* vertex) = 0;
+    virtual Edge<E>* addEdge(Vertex<V>* from, Vertex<V>* to, E weight) = 0;
+    virtual void removeEdge(Edge<E>* edge) = 0;
+    virtual vector<Edge<E>*> edgesFrom(Vertex<V>* vertex) = 0;
+    virtual vector<Edge<E>*> edgesTo(Vertex<V>* vertex) = 0;
+    virtual Vertex<V>* findVertex(V value) = 0;
+    virtual Edge<E>* findEdge(V from_value, V to_value) = 0;
+    virtual bool hasEdge(Vertex<V>* from, Vertex<V>* to) = 0;
+
+};
+
+
+template <typename V, typename E> class AdjacencyMatrixGraph : public GraphADT<V, E>{
+
+};
 
 
 
 int main() {
 
     
-
 
     return 0;
 
